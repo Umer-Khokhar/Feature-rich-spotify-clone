11fe8fdfa44adffc21bd8df37f3ac03556198e9b
Umer Khokhar <umerkhokhar1001@gmail.com>
2024-01-05 13:46:41 +0500

    responsiveness

--- main.js
+++ main.js
@@ -1,4 +1,21 @@
 import './style.css'
 
+const navShow = document.getElementById('nav-burger');
+const leftSide = document.querySelector('.left')
+const rightSide = document.querySelector('.right')
+const show = document.querySelector(".show")
+
+console.log(leftSide)
+
+navShow.addEventListener('click', e => {
+    leftSide.classList.add('active')
+    console.log("clicked")
+    rightSide.classList.add('unactive')
+})
 
 
+show.addEventListener('click', e => {
+    leftSide.classList.remove('active')
+    console.log("clicked")
+    rightSide.classList.remove('unactive')
+})
