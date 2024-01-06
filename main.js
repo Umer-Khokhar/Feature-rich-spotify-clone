5095c357cb356f0f70c74959abd2832edefea943
Umer Khokhar <umerkhokhar1001@gmail.com>
2024-01-06 15:27:26 +0500

    Initial commit

--- main.js
+++ main.js
@@ -1,21 +1,6 @@
 import './style.css'
 
-const navShow = document.getElementById('nav-burger');
-const leftSide = document.querySelector('.left')
-const rightSide = document.querySelector('.right')
-const show = document.querySelector(".show")
+import * as hamburger from './js/hamburder.js'
 
-console.log(leftSide)
-
-navShow.addEventListener('click', e => {
-    leftSide.classList.add('active')
-    console.log("clicked")
-    // rightSide.classList.add('unactive')
-})
-
-
-show.addEventListener('click', e => {
-    leftSide.classList.remove('active')
-    console.log("clicked")
-    rightSide.classList.remove('unactive')
-})
+hamburger.navShow.addEventListener('click', hamburger.openNav)
+hamburger.show.addEventListener('click', hamburger.closeNav)
